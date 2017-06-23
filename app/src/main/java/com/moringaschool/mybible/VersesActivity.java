13e3d3cc7fb4ab68a1384b6ef504dd90eab913f7
wmbotela <wmbotela@gmail.com>
2017-06-23 11:03:06 +0300

    Reading from Database working.

--- app/src/main/java/com/moringaschool/mybible/VersesActivity.java
+++ app/src/main/java/com/moringaschool/mybible/VersesActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -14,10 +15,19 @@ import android.widget.Toast;
 import android.widget.AdapterView;
 import android.view.View;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class VersesActivity extends AppCompatActivity {
+public class VersesActivity extends AppCompatActivity{
     @Bind(R.id.listView) ListView mListView;
 
 
@@ -25,6 +35,8 @@ public class VersesActivity extends AppCompatActivity {
     private String[] ecalls = new String[]{"Upset", "Lonely", "Need Direction", "Worried", "Anxious", "Unhappy", "In Danger", "Depressed", "Lack Faith",
             "Discouraged with Work", "Seeking Peace", "Leaving on a trip", "Struggling with Loss", "Struggling Financially"};
 
+    private DatabaseReference mDatabase;
+    private List<String> scriptures;
 
 
     @Override
@@ -34,15 +46,39 @@ public class VersesActivity extends AppCompatActivity {
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
         ButterKnife.bind(this);
+        
+
+        scriptures= new ArrayList<>();
 
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, ecalls);
+
+        ArrayAdapter adapter = new ArrayAdapter(this,R.layout.custom_list_item, ecalls);
         mListView.setAdapter(adapter);
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 String ecalls = ((TextView) view).getText().toString();
-                Toast.makeText(VersesActivity.this, ecalls, Toast.LENGTH_LONG).show();
+//                Toast.makeText(VersesActivity.this, ecalls, Toast.LENGTH_LONG).show();
+                mDatabase = FirebaseDatabase.getInstance().getReference().child("Scriptures");
+                final Integer itemPosition = i;
+                mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for (DataSnapshot snapshot: dataSnapshot.getChildren()){
+                            String scripture = snapshot.getValue(String.class);
+                            scriptures.add(scripture);
+                        }
+                        AlertDialog.Builder scripturedialogue = new AlertDialog.Builder(VersesActivity.this);
+                        scripturedialogue.setMessage(scriptures.get(itemPosition));
+                        scripturedialogue.show();
+                        scripturedialogue.setCancelable(true);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
             }
         });
     }
--- app/src/main/java/com/moringaschool/mybible/VersesArrayAdapter.java
+++ app/src/main/java/com/moringaschool/mybible/VersesArrayAdapter.java
@@ -11,17 +11,44 @@ import android.widget.TextView;
  * Created by william on 6/22/17.
  */
 
-public class VersesArrayAdapter {
-//    @Override
-//    public View getView(int position, View view, ViewGroup viewGroup)
-//   {
-//        View v = super.getView(position, view, viewGroup);
-//        Typeface beryliumFont = Typeface.createFromAsset(getAssets(), "fonts/Berylium.ttf");
-//        ((TextView)v).setTypeface(beryliumFont);
-//        return v;
+//public class VersesArrayAdapter extends ArrayAdapter  {
+//
+//    private Context mContext;
+//    private String[] mEcalls;
+//
+//
+//    public VersesArrayAdapter(Context mContext, int resource, String[] mEcalls) {
+//        super(mContext, resource);
+//        this.mContext = mContext;
+//        this.mEcalls = mEcalls;
+//
 //    }
-
-
-
-
-}
+//
+//    @Override
+//    public Object getItem(int position) {
+//        String ecall = mEcalls[position];
+//        return String.format("%s \n %s", ecall);
+//    }
+//
+//    @Override
+//    public int getCount() {
+//        return mEcalls.length;
+//    }
+//
+//
+//
+//
+//
+////    @Override
+////    public View getView(int position, View view, ViewGroup viewGroup)
+////   {
+////        View v = super.getView(position, view, viewGroup);
+////        Typeface beryliumFont = Typeface.createFromAsset(getAssets(), "fonts/Berylium.ttf");
+////        ((TextView)v).setTypeface(beryliumFont);
+////        return v;
+////    }
+//
+//
+//
+//
+//}
